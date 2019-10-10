6853cc4aec86f0d68544104d83a53c059c50191a
mvhk <harish.mamidi.kumar@gmail.com>
2019-10-10 20:51:08 +0530

    Basic routes are set

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{config("app.name","About-Jobportal")}}</title>
+</head>
+<body>
+    <h1>About page of the Job portal</h1>
+    <p>
+        THis is the job portal about things
+    </p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about',function(){
+    return view("pages.about");
 });
+Route::get("/","PagesController@index");
