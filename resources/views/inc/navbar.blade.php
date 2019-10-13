35ae0d4a20f05e40a24b98c751f2cc791ca6770f
mvhk <harish.mamidi.kumar@gmail.com>
2019-10-13 15:58:35 +0530

    Navbar fixed

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,15 +1,55 @@
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark text-white border-bottom box-shadow">
-    <h5 class="my-0 mr-md-auto font-weight-normal">{{config("app.name","Job Portal")}}</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
-              <a class="p-2 text-white" href="/">Home</a>
-              <a class="p-2 text-white" href="/about">About</a>
-              <a class="p-2 text-white" href="/services">Services</a>
-              <a class="p-2 text-white" href="/posts">Jobs</a> 
-              <a class="p-2 text-white" href="/posts/create">Create-Job</a> 
-              
-            </nav>
-            <a class="btn btn-outline-warning" href="/signup">Sign up</a>
-    </div>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>tinymce.init({selector:'textarea'});</script>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+      <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+              {{ config('app.name', 'Laravel') }}
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse " id="navbarSupportedContent">
+              <!-- Left Side Of Navbar -->
+              <ul class="navbar-nav mr-auto">
+                <a class="p-2 text-white" href="/">Home</a>
+                <a class="p-2 text-white" href="/about">About</a>
+                <a class="p-2 text-white" href="/services">Services</a>
+                <a class="p-2 text-white" href="/posts">Jobs</a> 
+                <a class="p-2 text-white" href="/posts/create">Create-Job</a> 
+              </ul>
+
+              <!-- Right Side Of Navbar -->
+              <ul class="navbar-nav ml-auto">
+                  <!-- Authentication Links -->
+                  @guest
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                      </li>
+                      @if (Route::has('register'))
+                          <li class="nav-item">
+                              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                          </li>
+                      @endif
+                  @else
+                      <li class="nav-item dropdown">
+                          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              {{ Auth::user()->name }} <span class="caret"></span>
+                          </a>
+
+                          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                  {{ __('Logout') }}
+                              </a>
+
+                              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  @csrf
+                              </form>
+                          </div>
+                      </li>
+                  @endguest
+              </ul>
+          </div>
+      </div>
+  </nav>
